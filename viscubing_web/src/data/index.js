a48f7cd33b415341aeb9747def6ae2cacbb289ad
Tya <fabulous0tya@gmail.com>
2021-05-08 13:08:15 +0800

    added data

---
+++ viscubing_web/src/data/index.js
@@ -0,0 +1,83 @@
+import best_2016 from "./best_2016.csv";
+import best_2017 from "./best_2017.csv";
+import best_2003 from "./best_2003.csv";
+import best_2015 from "./best_2015.csv";
+import best_2014 from "./best_2014.csv";
+import best_2010 from "./best_2010.csv";
+import best_2004 from "./best_2004.csv";
+import best_2005 from "./best_2005.csv";
+import best_2011 from "./best_2011.csv";
+import best_2007 from "./best_2007.csv";
+import best_2013 from "./best_2013.csv";
+import best_2012 from "./best_2012.csv";
+import best_2006 from "./best_2006.csv";
+import average_2020 from "./average_2020.csv";
+import average_2008 from "./average_2008.csv";
+import average_2009 from "./average_2009.csv";
+import average_2021 from "./average_2021.csv";
+import average_2019 from "./average_2019.csv";
+import average_2018 from "./average_2018.csv";
+import average_2016 from "./average_2016.csv";
+import average_2003 from "./average_2003.csv";
+import average_2017 from "./average_2017.csv";
+import average_2015 from "./average_2015.csv";
+import average_2014 from "./average_2014.csv";
+import average_2004 from "./average_2004.csv";
+import average_2010 from "./average_2010.csv";
+import average_2011 from "./average_2011.csv";
+import average_2005 from "./average_2005.csv";
+import average_2013 from "./average_2013.csv";
+import average_2007 from "./average_2007.csv";
+import average_2006 from "./average_2006.csv";
+import average_2012 from "./average_2012.csv";
+import best_2020 from "./best_2020.csv";
+import best_2008 from "./best_2008.csv";
+import best_2009 from "./best_2009.csv";
+import best_2021 from "./best_2021.csv";
+import best_2019 from "./best_2019.csv";
+import best_2018 from "./best_2018.csv";
+import continents_index from "./WCA_export_Continents.tsv"
+import countries_index from "./WCA_export_Countries.tsv"
+import bests from "./bests.csv";
+const index = {
+	best_2016: best_2016,
+	best_2017: best_2017,
+	best_2003: best_2003,
+	best_2015: best_2015,
+	best_2014: best_2014,
+	best_2010: best_2010,
+	best_2004: best_2004,
+	best_2005: best_2005,
+	best_2011: best_2011,
+	best_2007: best_2007,
+	best_2013: best_2013,
+	best_2012: best_2012,
+	best_2006: best_2006,
+	average_2020: average_2020,
+	average_2008: average_2008,
+	average_2009: average_2009,
+	average_2021: average_2021,
+	average_2019: average_2019,
+	average_2018: average_2018,
+	average_2016: average_2016,
+	average_2003: average_2003,
+	average_2017: average_2017,
+	average_2015: average_2015,
+	average_2014: average_2014,
+	average_2004: average_2004,
+	average_2010: average_2010,
+	average_2011: average_2011,
+	average_2005: average_2005,
+	average_2013: average_2013,
+	average_2007: average_2007,
+	average_2006: average_2006,
+	average_2012: average_2012,
+	best_2020: best_2020,
+	best_2008: best_2008,
+	best_2009: best_2009,
+	best_2021: best_2021,
+	best_2019: best_2019,
+	best_2018: best_2018,
+}
+export default index;
+export {bests, countries_index};
